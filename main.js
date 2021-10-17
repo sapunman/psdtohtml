9b6809a75490750ac22dce01ebfb20fe90a87635
Antonije Sapundzic <antonije.s@gmail.com>
2021-10-17 21:13:11 +0200

    nema js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-let burgerBtn = document.querySelector(".burger");
-let dropDown = document.querySelector(".nav-links");
 
-burgerBtn.addEventListener("click", () => {
-	// console.log("MENI OTVORI SE");
-	dropDown.classList.toggle("showNav");
-});
